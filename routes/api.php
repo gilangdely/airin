9dcfdb5ec506819f29994aa1fcbf20bfbfaf5592
gilangdmwork <85916106+gilangdely@users.noreply.github.com>
2025-06-11 08:09:16 +0700

    perbaikan ulang konflik

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,15 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
-<<<<<<< HEAD
+
 use App\Http\Controllers\API\HomeController;
 use App\Http\Controllers\API\PemakaianController;
 use Illuminate\Http\Request;
-=======
+
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PemakaianController;
 use App\Http\Controllers\TagihanController;
->>>>>>> 82537f59bf5e2d27fda70884935bf73f8181694c
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('storage/{path}', function ($path) {
